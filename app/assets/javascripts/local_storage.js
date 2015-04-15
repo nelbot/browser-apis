a215f7777c89f43af5c5c8b0dc464638d545c4a9
nelbot <nelson.cuadras@gmail.com>
2015-04-15 18:57:10 -0400

    added local storage

--- app/assets/javascripts/local_storage.js
+++ app/assets/javascripts/local_storage.js
@@ -1,39 +1,42 @@
-if (typeof window.localStorage === 'undefined')
-{
-  alert('Upgrade your browser, pops (or moms ???).')
-}
+// if (typeof window.localStorage === 'undefined')
+// {
+//   alert('Upgrade your browser, pops (or moms ???).')
+// }
  
-else
-{
-  // set items inside local storage
-  localStorage.setItem('key', 'value');
+// else
+// {
+//   // set items inside local storage
+//   localStorage.setItem('key', val);
  
-  // retrieve items from local storage
-  var val = localStorage.getItem('key');
+//   // retrieve items from local storage
+//   var val = localStorage.getItem('key');
  
-  console.debug('heres the value', val);
+//   console.debug('heres the value', val);
  
-  localStorage.setItem('nope', 'NOPE');
+//   localStorage.setItem('nope', 'NOPE');
  
-  // remove an item
-  localStorage.removeItem('nope');
+//   // remove an item
+//   localStorage.removeItem('nope');
  
-  // the item will be null if you try to get it after removing it
-  var nope = localStorage.getItem('nope');
+//   // the item will be null if you try to get it after removing it
+//   var nope = localStorage.getItem('nope');
  
-  console.debug('heres the value', val);
+//   console.debug('heres the value', val);
  
-  // remove all local storage keys
-  localStorage.clear();
+//   // remove all local storage keys
+//   localStorage.clear();
 
-};
+// };
+
+var dude = localStorage.getItem('key');
+$('body').addClass(dude);
 
 function change () {
-    
     var val = $('.js-dropdown').val();
     console.debug('Changed to', val);
     $('body').removeClass('chunk murray busey peter');
     $('body').addClass(val);
+    localStorage.setItem('key', val);
 
 }
 
